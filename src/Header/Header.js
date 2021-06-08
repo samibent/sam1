0dfb88e9ee3fc06c5bea173c63ec0b81acd0f9bf
samira <chsabrina11@gmail.com>
2021-06-08 11:51:28 +0200

    first commit

---
+++ src/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    const myStyle = {
+        backgroundColor = "pink"
+    }
+    return <div mstyle = { myStyle } > mon Header < /div>
+};
+export default Header;
